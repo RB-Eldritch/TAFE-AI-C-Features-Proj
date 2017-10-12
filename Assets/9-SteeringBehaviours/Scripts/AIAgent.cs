30b9fc21f4646c2befde7ba2a9e1ee95880d507b
RB-Eldritch <jamesreece1496@gmail.com>
2017-10-12 12:10:59 +1100

    Added 9-SteeringBehaviours

---
+++ Assets/9-SteeringBehaviours/Scripts/AIAgent.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace AI {
+
+    public class AIAgent : MonoBehaviour {
+
+        public Vector3 force;
+        public Vector3 velocity;
+        public float maxVelocity = 100f;
+        public float maxDistance;
+        public bool freezeRotation = false;
+
+        private NavMeshAgent nav;
+        private List<SteeringBehaviour> behaviours;
+
+
+
+        // Use this for initialization
+        void Start() {
+
+            behaviours = new List<SteeringBehaviour>(GetComponents<SteeringBehaviour>());
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+            ComputeForce();
+            ApplyVelocity();
+        }
+
+        void ComputeForce() {
+
+            //SET force = Vector3.zero
+            force = Vector3.zero;
+
+            //FOR i := 0 to behaviour count
+            for (int i = 0; i < behaviours.Count; i++) {
+
+                //LET Behaviour = behaviours[i]
+                SteeringBehaviour behaviour = behaviours[i];
+
+                //IF behaviour.setactive == false
+                if (behaviour.isActiveAndEnabled == false) {
+
+                    //continue
+                    continue;
+                }
+
+                //SET force = force + behaviour.GetForce() x weighting
+                force += behaviour.GetForce() * behaviour.weighting;
+
+                //IF force.magnitude > maxVelocity
+                if (force.magnitude < maxVelocity) {
+
+                    //SET force = force.normalized x maxVelocity
+                    force = force.normalized * maxVelocity;
+
+                    //break
+                    break;
+                }
+            }
+
+
+        }
+
+        void ApplyVelocity() {
+
+            //SET velocity = velocity + force x deltaTime
+            velocity = velocity + force * Time.deltaTime;
+
+            //IF velocity.magnitude > maxVelocity
+            if (velocity.magnitude > maxVelocity) {
+
+                //SET velocity = velocity.normalized x maxVelocity
+                velocity = velocity.normalized * maxVelocity;
+            }
+
+            //IF velocity.magnitude > 0
+            if (velocity.magnitude > 0) {
+
+                //SET transform.position = transform.position + velocity x deltaTime
+                transform.position += velocity * Time.deltaTime;
+
+                //SET transform.rotation = Quaternion LookRotation (velocity)
+                transform.rotation = Quaternion.LookRotation(velocity);
+            }
+
+
+
+        }
+    }
+}
---
+++ Assets/9-SteeringBehaviours/Scripts/SteeringBehaviours/Seek.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AI {
+
+    public class Seek : SteeringBehaviour {
+
+        public float stoppingDistance = 1f;
+
+        public Transform target;
+
+        public override Vector3 GetForce() {
+
+            Vector3 force = Vector3.zero;
+
+            if (target == null) {
+
+                return force;
+            }
+
+            //Let desiredForce = target position - transform position
+            Vector3 desiredForce = target.position - transform.position;
+
+            //If desiredForce magnitude > stoppingDistance
+            if (desiredForce.magnitude > stoppingDistance) {
+
+                //desiredForce = desiredForce normalized x weighting
+                desiredForce = desiredForce.normalized * weighting;
+
+                //force = desiredForce - owner.velocity
+                force = desiredForce - owner.velocity;
+
+            }
+
+            //return force
+            return force;
+        }
+    }
+}
