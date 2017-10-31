c80ad06ca857213796be1bdc1defb8aad5d14cfa
RB-Eldritch <jamesreece1496@gmail.com>
2017-10-31 16:16:22 +1100

    Added the beginnings of the Asteroids Game

---
+++ Assets/~Asteroids/Scripts/Shooting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroid {
+
+    public class Shooting : MonoBehaviour {
+
+        public GameObject bulletPrefab;
+        public float bulletSpeed = 20f;
+        public float shootRate = .2f;
+
+        private float shootTimer = 0f;
+
+        // Update is called once per frame
+        void Update() {
+
+            shootTimer += Time.deltaTime;
+
+            if (shootTimer > shootRate) {
+
+                if (Input.GetKey(KeyCode.Space)) {
+
+                    Shoot();
+                    shootTimer = 0;
+                }
+            }
+        }
+
+        void Shoot() {
+
+            GameObject bulletClone = Instantiate(bulletPrefab);
+            Rigidbody2D cloneRigid = bulletClone.GetComponent<Rigidbody2D>();
+            cloneRigid.AddForce(transform.up * bulletSpeed, ForceMode2D.Impulse);
+        }
+    }
+}
