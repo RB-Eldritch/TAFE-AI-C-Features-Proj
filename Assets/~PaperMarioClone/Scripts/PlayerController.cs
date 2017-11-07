d400d1e098ed0611de35232df6b95f0c619c3dee
RB-Eldritch <jamesreece1496@gmail.com>
2017-11-07 16:03:15 +1100

    Added first part of PaperMarioClone Lesson

---
+++ Assets/~PaperMarioClone/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PaperMarioClone {
+
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerController : MonoBehaviour {
+
+        public float walkSpeed = 20f;
+        public float runSpeed = 30f;
+        public float jumpHeight = 10f;
+
+        public bool moveInJump = false;
+        public bool isRunning = false;
+
+
+        private Vector3 gravity;
+        private Vector3 movement;
+        [HideInInspector] public Vector3 inputDir;
+
+        private bool jump = false;
+
+        private CharacterController controller;
+
+        public bool isGrounded {
+
+            get { return controller.isGrounded; }
+        }
+
+
+        // Use this for initialization
+        void Start() {
+
+            controller = GetComponent<CharacterController>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+            if (isRunning) 
+                movement *= runSpeed;
+            else 
+                movement *= walkSpeed;
+
+            if (isGrounded) {
+
+                gravity = Vector3.zero;
+
+                if (jump) {
+
+                    gravity.y = jumpHeight;
+                    jump = false;
+                }
+            }
+            else {
+
+                gravity += Physics.gravity * Time.deltaTime;
+            }
+
+            movement += gravity;
+            controller.Move(movement * Time.deltaTime);
+        }
+
+        public void Jump() {
+
+            jump = true;
+        }
+
+        public void Move(float inputH, float InputV) {
+
+            if (moveInJump || (!moveInJump && isGrounded)) {
+
+                inputDir = new Vector3(inputH, 0, InputV);
+            }
+
+            movement = inputDir;
+        }
+    }
+}
---
+++ Assets/~PaperMarioClone/Scripts/UserInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PaperMarioClone {
+
+    [RequireComponent(typeof(PlayerController))]
+    public class UserInput : MonoBehaviour {
+
+        private PlayerController pController;
+
+
+        // Use this for initialization
+        void Start() {
+
+            pController = GetComponent<PlayerController>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+            float inputH = Input.GetAxis("Horizontal");
+            float inputV = Input.GetAxis("Vertical");
+
+            pController.Move(inputH, inputV);
+
+            if (Input.GetKeyDown(KeyCode.Space)) {
+
+                pController.Jump();
+            }
+        }
+    }
+}
